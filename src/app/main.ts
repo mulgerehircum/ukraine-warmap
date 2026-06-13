538420580d5bf95cf2abb07eb4b19d9c46d9d418
Andriy Ponomarienko <112684452+mulgerehircum@users.noreply.github.com>
2026-06-13 16:59:27 +0300

    add vercel analytics

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import 'maplibre-gl/dist/maplibre-gl.css'
 import '../shared/styles/aero.css'
 import App from './App.vue'
+import { inject } from '@vercel/analytics'
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/sw.js').catch(() => {/* non-fatal */})
 }
+
+inject()
 
 const app = createApp(App)
 app.config.devtools = true
